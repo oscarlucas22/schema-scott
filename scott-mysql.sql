2e6f7db86cc4670816a4b7ed708af9ba4a19f401
Óscar Lucas <98739924+oscarlucas22@users.noreply.github.com>
2023-03-01 21:06:06 +0100

    Create scott-mysql.sql

---
+++ scott-mysql.sql
@@ -0,0 +1,79 @@
+-- Creación de las tablas de SCOTT
+
+create table dept (
+        deptno integer,
+        dname  text,
+        loc    text,
+        constraint pk_dept primary key (deptno)
+);
+
+create table emp (
+        empno    integer,
+        ename    text,
+        job      text,
+        mgr      integer,
+        hiredate date,
+        sal      integer,
+        comm     integer,
+        deptno   integer,
+        constraint pk_emp primary key (empno),
+        constraint fk_mgr foreign key (mgr) references emp (empno),
+        constraint fk_deptno foreign key (deptno) references dept (deptno)
+);
+
+create table salgrade (
+        grade integer,
+        losal integer,
+        hisal integer,
+        constraint pk_salgrade primary key (grade)
+);
+
+create table bonus (
+        empno integer,
+        bonus integer,
+        constraint pk_bonus primary key (empno),
+        constraint fk_empno foreign key (empno) references emp (empno)
+);
+
+-- Inserción de datos en las tablas
+
+insert into dept values (10, 'ACCOUNTING', 'NEW YORK');
+insert into dept values (20, 'RESEARCH', 'DALLAS');
+insert into dept values (30, 'SALES', 'CHICAGO');
+insert into dept values (40, 'OPERATIONS', 'BOSTON');
+
+insert into emp values (7369, 'SMITH', 'CLERK', 7902, '1980-12-17', 800, null, 20);
+insert into emp values (7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600, 300, 30);
+insert into emp values (7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250, 500, 30);
+insert into emp values (7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975, null, 20);
+insert into emp values (7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250, 1400, 30);
+insert into emp values (7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850, null, 30);
+insert into emp values (7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450, null, 10);
+insert into emp values (7788, 'SCOTT', 'ANALYST', 7566, '1987-04-19', 3000, null, 20);
+insert into emp values (7839, 'KING', 'PRESIDENT', null, '1981-11-17', 5000, null, 10);
+insert into emp values (7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', 1500, 0, 30);
+insert into emp values (7876, 'ADAMS', 'CLERK', 7788, '1987-05-23', 1100, null, 20);
+insert into emp values (7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950, null, 30);
+insert into emp values (7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000, null, 20);
+insert into emp values (7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300, null, 10);
+
+insert into salgrade values (1, 700, 1200);
+insert into salgrade values (2, 1201, 1400);
+insert into salgrade values (3, 1401, 2000);
+insert into salgrade values (4, 2001, 3000);
+insert into salgrade values (5, 3001, 9999);
+
+insert into bonus values (7369, 100);
+insert into bonus values (7499, 200);
+insert into bonus values (7521, 300);
+insert into bonus values (7566, 400);
+insert into bonus values (7654, 500);
+insert into bonus values (7698, 600);
+insert into bonus values (7782, 700);
+insert into bonus values (7788, 800);
+insert into bonus values (7839, 900);
+insert into bonus values (7844, 1000);
+insert into bonus values (7876, 1100);
+insert into bonus values (7900, 1200);
+insert into bonus values (7902, 1300);
+insert into bonus values (7934, 1400);
